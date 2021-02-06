de91415385c90570f0a2bc2085e0327595030675
Zekeriya İshak DOĞANAY <zekishakdgny38@gmail.com>
2021-02-07 00:21:59 +0300

    06.02.2021

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -7,15 +7,18 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     // Context: Db tabloları ile proje classlarını bağlamak.
-    public class NortwindContext:DbContext
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
         }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,15 @@ namespace DataAccess.Concrete.InMemory
     public class InMemoryProductDal : IProductDal
     {
         List<Product> _products;
-        //ctor
         public InMemoryProductDal()
         {
+            //Oracle,Sql Server, Postgres , MongoDb
             _products = new List<Product> {
-                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice =15,UnitsInStock=15},
-                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice =500,UnitsInStock=3},
-                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice =1500,UnitsInStock=2 },
-                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice =150,UnitsInStock=65},
-                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice =85,UnitsInStock=1}
-
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
             };
         }
         public void Add(Product product)
@@ -44,17 +44,17 @@ namespace DataAccess.Concrete.InMemory
 
         public void Update(Product product)
         {
-            // Gönderdiğim ürün Id'sine sahip olan Listedeki ürünü bul.
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
-
         }
+
         public List<Product> GetAllByCategory(int categoryId)
         {
-           return _products.Where(p => p.CategoryId == categoryId).ToList();
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
@@ -63,6 +63,11 @@ namespace DataAccess.Concrete.InMemory
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
         {
             throw new NotImplementedException();
         }
